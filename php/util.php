54c26e221c689044ed6037089011ac23d0ac0cbc
Paulo Cézar Júnior <paulocezar.francisco.junior@gmail.com>
2019-01-04 00:37:13 -0200

    Add Cache Control to not save cache

--- php/util.php
+++ php/util.php
@@ -1,5 +1,9 @@
 <?php
 
+header("Cache-Control: no-cache, must-revalidate"); //HTTP 1.1
+header("Pragma: no-cache"); //HTTP 1.0
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
+
 /**
  *
  * @param mixed $item
