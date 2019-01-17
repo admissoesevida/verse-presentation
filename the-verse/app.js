ca7b6282951f77e86df169fbbee575300850040f
paulojunior95 <paulocezar.francisco.junior@gmail.com>
2019-01-17 08:27:41 -0200

    Updated The Verse page to change Image for each verse

--- the-verse/app.js
+++ the-verse/app.js
@@ -1,11 +1,26 @@
 let CURRENT_VERSE = '';
+let CURRENT_IMAGE_INDEX = 1;
+const MAX_IMAGE_INDEX = 5;
+
 const updateCurrentVerse = async () => {
   let nextVerse = await fetchVerse();
 
   if (nextVerse && !CURRENT_VERSE.equalsTo(nextVerse)) {
     CURRENT_VERSE = nextVerse;
     await updateDom();
+    changeBackgroundImage();
   }
+};
+
+const changeBackgroundImage = () => {
+  const background = document.querySelector('#background');
+
+  if (CURRENT_IMAGE_INDEX + 1 > MAX_IMAGE_INDEX) {
+    CURRENT_IMAGE_INDEX = 1;
+  }
+
+  background.style.backgroundImage =
+    'url(img/' + ++CURRENT_IMAGE_INDEX + '.jpg)';
 };
 
 const main = async () => {
