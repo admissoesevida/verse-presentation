150a323822ff330bedd4468208cf3ccfa5ae9ea7
Paulo Cézar Júnior <paulocezar.francisco.junior@gmail.com>
2019-01-19 01:37:04 -0200

    Includes credentials sample file

---
+++ php/credentials.sample.php
@@ -0,0 +1,28 @@
+<?php
+
+$credentials = [
+  "biblia-app" => [
+    "host" => "localhost",
+    "user" => "database-user",
+    "pass" => "your-password-goes-here",
+    "db" => "biblia-app"
+  ],
+  "biblia-aa" => [
+    "host" => "localhost",
+    "user" => "database-user",
+    "pass" => "your-password-goes-here",
+    "db" => "biblia-aa"
+  ],
+  "biblia-acf" => [
+    "host" => "localhost",
+    "user" => "database-user",
+    "pass" => "your-password-goes-here",
+    "db" => "biblia-acf"
+  ],
+  "biblia-nvi" => [
+    "host" => "localhost",
+    "user" => "database-user",
+    "pass" => "your-password-goes-here",
+    "db" => "biblia-nvi"
+  ]
+];
